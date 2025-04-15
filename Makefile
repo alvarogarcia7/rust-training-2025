e1afddedbc543e8faaa5441adfbd441fe5d020e5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-15 12:15:04 +0400

    add doctests for the calc module

--- Makefile
+++ Makefile
@@ -5,6 +5,10 @@ test:
 	cargo test --all --all-features --tests
 .PHONY: test
 
+test-doc:
+	cargo test --doc
+.PHONY: test-doc
+
 clippy:
 	cargo clippy --all --all-features --tests -- -D warnings
 .PHONY: clippy
--- p22/src/calc.rs
+++ p22/src/calc.rs
@@ -1,7 +1,22 @@
+/// Convert Celsius to Fahrenheit degrees
+///
+/// Note: This function rounds to the nearest integer, half up. https://en.wikipedia.org/wiki/Rounding#Rounding_half_up
+///
+/// ```
+/// use p22::calc::celsius2fahrenheit;
+/// assert_eq!(celsius2fahrenheit(0), 32);
+/// ```
 pub fn celsius2fahrenheit(celsius: i32) -> i32 {
     let x: f32 = (celsius as f32 * 9f32) / 5f32;
     x.round() as i32 + 32
 }
+
+/// Convert Fahrenheit to Celsius degrees
+///
+/// ```
+/// use p22::calc::fahrenheit2celsius;
+/// assert_eq!(fahrenheit2celsius(32), 0);
+/// ```
 pub fn fahrenheit2celsius(fahrenheit: i32) -> i32 {
     (5 * (fahrenheit - 32)) / 9
 }
