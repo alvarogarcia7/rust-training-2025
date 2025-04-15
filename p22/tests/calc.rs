cf17c6036a0bb546ddbad8d09f437a03cd617da5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-15 11:54:08 +0400

    green: more test cases from https://weather.dfrc.nasa.gov/data/ctof.pdf

--- p22/tests/calc.rs
+++ p22/tests/calc.rs
@@ -3,7 +3,19 @@ mod tests {
     use p22::calc::celsius2fahrenheit;
 
     #[test]
-    fn test_celsius2fahrenheit() {
+    fn test_celsius2fahrenheit_at_0() {
         assert_eq!(celsius2fahrenheit(0), 32);
+    }
+    #[test]
+    fn test_celsius2fahrenheit_at_2() {
+        assert_eq!(celsius2fahrenheit(2), 35);
+    }
+    #[test]
+    fn test_celsius2fahrenheit_at_20() {
+        assert_eq!(celsius2fahrenheit(20), 68);
+    }
+    #[test]
+    fn test_celsius2fahrenheit_at_minus_15() {
+        assert_eq!(celsius2fahrenheit(-15), 5);
     }
 }
