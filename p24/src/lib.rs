634a44ec296078d6a980849bd365d65d2f951215
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-15 16:16:48 +0400

    p24: green for f1 (clippy errors)

--- p24/src/lib.rs
+++ p24/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn f1<'a>(a: &'a mut (u32, u32), b: bool) -> &'a mut u32 {
+    if !b { &mut a.0 } else { &mut a.1 }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn f1_true() {
+        let i0 = 0;
+        let i1 = 1;
+        let tuple = &mut (i0, i1);
+
+        let result = f1(tuple, true);
+
+        assert_eq!(*result, 1);
+    }
+    #[test]
+    fn f1_false() {
+        let i0 = 0;
+        let i1 = 1;
+        let tuple = &mut (i0, i1);
+
+        let result = f1(tuple, false);
+
+        assert_eq!(*result, 0);
     }
 }
