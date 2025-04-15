32281bb6f7158f02a9653628ff25ca520eacace7
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-15 13:47:33 +0400

    add basic benchmark testing
    Signed-off-by: Alvaro Garcia <alvarogarcia7@users.noreply.github.com>

--- p22/tests/benchmark.rs
+++ p22/tests/benchmark.rs
@@ -3,12 +3,16 @@
 extern crate test;
 
 #[cfg(test)]
-mod tests {
-    use p22::calc::celsius2fahrenheit;
+mod tests_benchmark_fibonacci {
+    use p22::calc::{fibonacci_loop, fibonacci_rec};
     use test::Bencher;
 
     #[bench]
-    fn bench_add_two(b: &mut Bencher) {
-        b.iter(|| celsius2fahrenheit(test::black_box(1000)));
+    fn bench_rec(b: &mut Bencher) {
+        b.iter(|| fibonacci_rec(test::black_box(30)));
+    }
+    #[bench]
+    fn bench_loop(b: &mut Bencher) {
+        b.iter(|| fibonacci_loop(test::black_box(30)));
     }
 }
