6420cf481a777a0ecbbc6a379f02681af6a425c9
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-15 16:21:22 +0400

    p24: green for f2

--- p24/src/lib.rs
+++ p24/src/lib.rs
@@ -1,6 +1,11 @@
 fn f1(a: &mut (u32, u32), b: bool) -> &mut u32 {
     if !b { &mut a.0 } else { &mut a.1 }
 }
+
+fn f2(a: &mut [u32], n: usize) -> &mut u32 {
+    &mut a[n]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +29,25 @@ mod tests {
         let result = f1(tuple, false);
 
         assert_eq!(*result, 0);
+    }
+    #[test]
+    #[allow(clippy::useless_vec)]
+    fn f2_0() {
+        let mut input = vec![0, 1, 2, 3];
+        let slice = &mut input[0..3];
+
+        let result = f2(slice, 0);
+
+        assert_eq!(*result, 0);
+    }
+    #[test]
+    #[allow(clippy::useless_vec)]
+    fn f2_2() {
+        let mut input = vec![0, 1, 2, 3];
+        let slice = &mut input[0..3];
+
+        let result = f2(slice, 2);
+
+        assert_eq!(*result, 2);
     }
 }
