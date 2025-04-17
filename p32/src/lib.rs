20927f29b65fbbdfd57bdd48e385bd1eeb4e09c8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-17 11:47:16 +0400

    p32: User.balance i64

--- p32/src/lib.rs
+++ p32/src/lib.rs
@@ -1,11 +1,16 @@
 struct User {
     name: String,
     credit_line: u64,
+    balance: i64,
 }
 
 impl User {
-    pub(crate) fn new(name: String, credit_line: u64) -> Self {
-        User { name, credit_line }
+    pub(crate) fn new(name: String, credit_line: u64, balance: i64) -> Self {
+        User {
+            name,
+            credit_line,
+            balance,
+        }
     }
 }
 
@@ -15,9 +20,10 @@ mod tests {
 
     #[test]
     fn user_constructor_fields() {
-        let user = User::new("Name Surname".to_string(), 4u64);
+        let user = User::new("Name Surname".to_string(), 4u64, -1i64);
 
         assert_eq!(user.name, "Name Surname".to_string());
         assert_eq!(user.credit_line, 4u64);
+        assert_eq!(user.balance, -1i64);
     }
 }
