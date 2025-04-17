5fea8a2ffd8c4d50e805f5744beaa2beb3db2612
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-17 11:35:21 +0400

    p24: solve bad lifetimes + turn main into test

--- p24/src/bad_lifetimes.rs
+++ p24/src/bad_lifetimes.rs
@@ -1,46 +1,53 @@
-fn main() {
-    let arr = [2, 4, 8, 16];
-
-    let mut n = 2;
-    let nth = nth_item(&arr, &n);
-    let increased = increased_by_first_item(&arr, &mut n);
-
-    let value = {
-        let values = TwoValues::new(&arr[3], increased);
-
-        assert_eq!(*values.get_first(), 16);
-
-        values.get_second()
-    };
-
-    assert_eq!(*value, 4);
-    assert_eq!(*nth, 8);
-}
-
-fn nth_item(data: &[usize], n: &usize) -> &usize {
+fn nth_item<'a>(data: &'a [usize], n: &usize) -> &'a usize {
     &data[*n]
 }
 
-fn increased_by_first_item(data: &[usize], n: &mut usize) -> &mut usize {
+fn increased_by_first_item<'b>(data: &[usize], n: &'b mut usize) -> &'b mut usize {
     *n += data[0];
     n
 }
 
-struct TwoValues {
-    first: &usize,
-    second: &usize,
+struct TwoValues<'a> {
+    first: &'a usize,
+    second: &'a usize,
 }
 
-impl TwoValues {
-    pub fn new(first: &usize, second: &usize) -> Self {
+impl<'a> TwoValues<'a> {
+    pub fn new(first: &'a usize, second: &'a usize) -> Self {
         Self { first, second }
     }
 
-    pub fn get_first(&self) -> &usize {
+    pub fn get_first(&self) -> &'a usize {
         self.first
     }
 
-    pub fn get_second(&self) -> &usize {
+    pub fn get_second(&self) -> &'a usize {
         self.second
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::bad_lifetimes::{TwoValues, increased_by_first_item, nth_item};
+
+    #[test]
+    pub fn bad_lifetimes() {
+        let arr = [2, 4, 8, 16];
+
+        let mut n = 2;
+        let nth = nth_item(&arr, &n);
+        let increased = increased_by_first_item(&arr, &mut n);
+
+        let value = {
+            let first = &arr[3];
+            let values = TwoValues::new(first, increased);
+
+            assert_eq!(*values.get_first(), 16);
+
+            values.get_second()
+        };
+
+        assert_eq!(*value, 4);
+        assert_eq!(*nth, 8);
     }
 }
