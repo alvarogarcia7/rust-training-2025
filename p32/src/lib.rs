014330d21c8c4ad9efa827561157780baf2d9ec3
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-19 13:42:46 +0400

    bankhelper - use it for assertion

--- p32/src/lib.rs
+++ p32/src/lib.rs
@@ -224,6 +224,8 @@ mod tests {
         assert!(result.is_err());
 
         assert_eq!(bank.calc_balance().assets, 1u64);
+        let bank_helper = BankHelper { bank: &bank };
+        assert_eq!(bank_helper.balance_sheet_for("name1"), Balance::new(1i64));
         assert_eq!(
             bank.balance_of_user("name1".to_string()),
             Balance::new(1i64)
