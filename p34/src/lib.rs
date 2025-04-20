67c9a5a3d90d03e4fc506bc8050c36a5634d130c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-04-20 21:49:33 +0400

    add with carry + import from Vec<u64>

--- p34/src/lib.rs
+++ p34/src/lib.rs
@@ -24,8 +24,15 @@ impl ops::Add<BigUint4096> for BigUint4096 {
     fn add(self, _rhs: BigUint4096) -> BigUint4096 {
         let mut values = self.values;
         // for i in 0..64 {
+        let mut carry = false;
         for (i, item) in values.iter_mut().enumerate() {
-            *item += _rhs.values[i];
+            let mut carry_from_first = false;
+            let carry_from_second;
+            if carry {
+                (*item, carry_from_first) = item.overflowing_add(1);
+            }
+            (*item, carry_from_second) = item.overflowing_add(_rhs.values[i]);
+            carry = carry_from_first || carry_from_second;
         }
         BigUint4096 { values }
     }
@@ -39,25 +46,45 @@ impl From<u64> for BigUint4096 {
     }
 }
 
+impl From<Vec<u64>> for BigUint4096 {
+    fn from(value: Vec<u64>) -> Self {
+        let mut values = [0; 64];
+        values[..value.len()].copy_from_slice(&value[..]);
+        // for i in 0..value.len() {
+        //     values[i] = value[i];
+        // }
+        Self { values }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn add_0_0() {
-        assert_eq_biguint4096(0, add(0,0));
+        assert_eq_biguint4096(0, add(0, 0));
     }
 
     #[test]
     fn add_0_1() {
         assert_eq_biguint4096(1, add(0, 1));
     }
+    #[test]
+    fn add_overflow_first_element() {
+        assert_eq!(BigUint4096::from(vec![0u64, 1u64]), add(u64::MAX, 1));
+    }
 
     fn assert_eq_biguint4096(expected: u64, actual: BigUint4096) {
         assert_eq!(BigUint4096::from(expected), actual);
     }
 
+    /// Create a BigUint4096 from a smaller number
+    fn bu(value: u64) -> BigUint4096 {
+        BigUint4096::from(value)
+    }
+
     fn add(p0: u64, p1: u64) -> BigUint4096 {
-        BigUint4096::from(p0) + BigUint4096::from(p1)
+        bu(p0) + BigUint4096::from(p1)
     }
 }
